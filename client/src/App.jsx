3e3fdf554e4b07ff79632580b4618b49593ff2d7
sneha <sneha.2327cs1220@kiet.edu>
2025-07-25 11:20:16 +0530

    update home import in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes, useLocation } from "react-router-dom"
 import Navbar from "./Component/Navbar"
-import Home from "./pages/home"
+import Home from "./pages/Home"
 import Movie from "./pages/Movie"
 import MovieDetails from "./pages/MovieDetails"
 import MyBooking from "./pages/MyBooking"
